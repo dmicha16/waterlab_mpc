42dfe70df9a239bc52a66afec8a0d5476a1323c4
Casper Knudsen <ckn16@student.aau.dk>
2020-04-23 11:24:13 +0200

    Made minor changes and added comments to gen_mpc_solver

--- networkcontrol.py
+++ networkcontrol.py
@@ -1,34 +1,37 @@
 """
 Module for Level control
-
 """
+
 import casadi as ca
 import  mpc
-def gen_mpc_solver(A, B, Hu, Hp, Q, R)
 
-    U = ca.SX(B.shape[1], Hp)
+def gen_mpc_solver(A, B, Hu, Hp, Q, R):
     # x0, u_prev, ref as SX
-
-    for t in range(0, Hp):
-        if t < Hu:
-            U[:, t] = ca.sum2(dU[:, 0:t + 1]) + u_prev
-        else:
-            U[:, t] = U[:, t - 1]
 
     dU = ca.SX.sym('dU', B.shape[1] * Hu, 1)
 
+    # To formulate a MPC optimization problem we need to describe:
+    # Z = psi x(k) + upsilon u(k-1) + Theta dU(x) (Assuming no disturbance)
     psi = mpc.gen_psi(A, Hp)
     upsilon = mpc.gen_upsilon(A, B, Hp)
     theta = mpc.gen_theta(upsilon, B, Hu)
-
     predicted_states = mpc.gen_predicted_states(psi, x0, upsilon, u_prev, theta, dU)
-    error = predicted_states - ref
+
+    # Cost function:
+    # Cost = (Z - T)' * Q * (Z - T) + dU' * R * dU;
+    error = predicted_states - ref # e = (Z - T)
     quadratic_cost = error.T @ Q @ error + dU.T @ R @ dU
     quadratic_problem = {'x': dU, 'f': quadratic_cost}
 
     mpc_solver = ca.qpsol('mpc_solver', 'qpoases', quadratic_problem)
     print(mpc_solver)
 
+    # U = ca.SX(B.shape[1], Hp)
+    # for t in range(0, Hp):
+    #     if t < Hu:
+    #         U[:, t] = ca.sum2(dU[:, 0:t + 1]) + u_prev
+    #     else:
+    #         U[:, t] = U[:, t - 1]
     return mpc_solver
 
 # def mpc_version_1(pump,tank):
