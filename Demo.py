bec8ac8f7c8fec62b22308f1dc7c2432e27984d1
frizzerdk <frizzer@live.dk>
2020-05-08 15:33:47 +0200

    Ready for testing MPC Object

--- Demo.py
+++ Demo.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+plt.ion()
 
 # Real Model
 A = ca.DM([[1, 0], [0, 1.5]])
@@ -12,6 +13,7 @@ B = ca.DM([[0.1, 0], [0, 0.5]])
 
 states = A.size1()
 inputs = B.size2()
+
 # Predictive Model
 rand_var = 0.1
 const_var = 0.9
@@ -30,11 +32,12 @@ Rb = mpc.blockdiag(R, Hu)
 x0 = ca.DM([[10], [11]])
 u0 = ca.DM([-1, -3])
 ref = ca.DM.ones(Hp * 2, 1)
+ref[1::A.size1()] = ref[1::A.size1()] + 2
 # ref[1::A.size1()] = np.cumsum(ref[1::A.size1()]) / 20 + 2
 
 
 mmpc = mpco.MpcObj(Ap, Bp, Hu, Hp, Q, R, x0, u0, ref)
-mmpc.plot_step({'drawU': 'U'})
+mmpc.plot_progress({'drawU': 'U'})
 
 steps = 50
 
@@ -47,38 +50,19 @@ looper = True
 
 for j in range(1, steps):
 
+
     if looper:
         loop_in = input("press any key to step, or \'r\' to run all steps")
         looper = 'r' != loop_in
 
     u = u + mmpc.get_next_control_input_change()
-    u_array = ca.horzcat(u_array, u)
     x = A @ x + B @ u
-    x_array = ca.horzcat(x_array, ca.vec(x))
+
     mmpc.step(x, u, ref)
-    mmpc.plot_step({'drawU': 'U'})
-    mmpc.plot_progress({'drawU': 'U'})
+    mmpc.plot_step({'drawU': 'both'})
+    #mmpc.plot_progress(options={'drawU': 'U'}, ignore_inputs=[1])
         # draw
 
 
 
 
-# draw
-t = range(0, j + 1)
-
-ax1 = plt.subplot(211)
-for s in range(0, states):
-    col = plt.cm.tab10(s)
-    start = 0
-
-    plt.plot(range(0, mmpc.get_k())[start:], mmpc.get_x0_log()[s, start:].T, color=col)
-    plt.plot(range(0, mmpc.get_k())[start:], mmpc.get_expected_x_log()[s, start:].T, '--', color=col)
-
-plt.subplot(212, sharex=ax1)
-
-t = np.append(np.arange(-1, 0), t)
-for i in range(0, inputs):
-    col = plt.cm.tab10(i)
-    plt.plot(range(0, mmpc.get_k() ), mmpc.get_dU_log()[i, :].T)
-
-plt.show()
--- mpco.py
+++ mpco.py
@@ -6,7 +6,20 @@ import matplotlib.pyplot as plt
 
 class MpcObj:
 
-    def __init__(self, A, B, Hu, Hp, Q, R, x0, u_prev, ref, saved_steps=30, k=1):
+    def __init__(self, A, B, Hu, Hp, Q, R, x0, u_prev, ref, log_length=0, k=1):
+        """
+        :param A:(mxm) Model dynamics matrix of type casadi.DM
+        :param B:(mxn) Input dynamics matrix of type casadi.DM
+        :param Hu:(int) Control horizon of type Integer
+        :param Hp: (int) Prediction horizon of type Integer
+        :param Q:(mxm) State cost matrix of type casadi.DM
+        :param R:(mxm) Input change cost matrix of type casadi.DM
+        :param x0:(mx1) Initial states
+        :param u_prev:(nx1) Initial contol input
+        :param ref: (m*hU x 1) Reference trajectory
+        :param log_length: (int) Nr. of steps to log, 0 = all
+        :param k: (int) Initial time step
+        """
 
         self.states = A.size1()
         self.inputs = B.size2()
@@ -37,11 +50,11 @@ class MpcObj:
         self.predicted_states = mpc.gen_predicted_states(self.psi, self.x0, self.upsilon, self.u_prev, self.theta,
                                                          self.dU)
 
-        self.saved_steps = saved_steps
+        self.log_length = log_length
         self.log_x0 = ca.vec(x0)
         self.log_ref = ca.vec(self.get_next_ref())
         self.log_expected_x = ca.vec(self.get_next_expected_state())
-        self.log_dU = ca.vec(self.get_next_control_input_change())
+        self.log_dU = ca.horzcat(ca.vec(u_prev), ca.vec(self.get_next_control_input_change()))
 
     def get_next_ref(self):
         return self.ref[:self.states]
@@ -163,19 +176,22 @@ class MpcObj:
         #   Inputs
         #   (Costs: dU og State cost)
 
-        opts = {'drawU': 'dU'}
+        plt.clf()
+
+        opts = {'drawU': 'U'}
         opts.update(options)
 
         t = np.arange(0, (self.predicted_states.size1() + 1) / 2)
 
         ax1 = plt.subplot(211)
+        plt.title("Optimized step at time k = {}".format(self.k))
         for s in range(0, self.states):
             col = plt.cm.tab10(s)
 
             plt.plot(t[1:], self.predicted_states[s::self.states], color=col)
             plt.plot(t[1:], self.ref[s::self.states], '--', color=col)
             plt.plot(t[0:2], ca.vertcat(self.x0[s], self.predicted_states[s]), 'r-')
-            plt.plot(t[0], self.x0[s], '.', color=col, label=s)
+            plt.plot(t[0], self.x0[s], '.', color=col, label="State {}={:.3f}".format(s, float(self.x0[s])))
 
         plt.ylabel('States')
         ax1.legend()
@@ -186,27 +202,30 @@ class MpcObj:
             col = plt.cm.tab10(i)
             w = 'post'
 
-            if opts['drawU'] == 'U':
+            if opts['drawU'] == 'both' or opts['drawU'] == 'U':
 
                 U = np.cumsum(ca.vertcat(self.u_prev[i], self.dU[i::self.inputs]))
 
                 plt.step(t[0:self.Hu + 1], U, color=col, where=w)
                 #       plt.step(t[1:3], U[1:3], color=col, where=w)
-                plt.plot(t[1], U[1], '.', color=col, label=i)
+                plt.plot(t[1], U[1], '.', color=col,label="u{}={:.3f}".format(i, float(U[1])))
 
                 plt.step(t[1:3], U[0:2], 'r-', where="pre")
 
-
+            if opts['drawU'] == 'both' or opts['drawU'] == 'dU':
                 idU = self.dU[i::self.inputs]
-                plt.plot(t[1:self.Hu+1], idU, '.', color=col)
+                plt.plot(t[1:self.Hu + 1], idU, '.', color=col)
                 # plt.plot(t[0:2], ca.vertcat(self.u_prev[i], idU[0]), '.', color=col)
-                plt.plot(t[1], idU[0], 'o', color=col, label="{}={}".format(i, idU[1]))
+                plt.plot(t[1], idU[0], 'o', color=col, label="du{}={:.3f}".format(i, float(idU[1])))
                 # plt.plot(t[0], idU[0], 'r.')
-            else:
-                print('k')
+
+
         ax2.legend()
+
         plt.xlabel('Time in steps')
         plt.ylabel(opts['drawU'])
+        plt.draw()
+        plt.pause(0.1)
         plt.show()
 
     def plot_progress(self, options={}, ignore_states=[], ignore_inputs=[], prev_n=20):
@@ -216,12 +235,15 @@ class MpcObj:
         #   Inputs
         #   (Costs: dU og State cost)
 
-        opts = {'drawU': 'dU'}
+        plt.clf()
+
+        opts = {'drawU': 'U'}
         opts.update(options)
 
         t = np.arange(-self.k, (self.predicted_states.size1() + 1) / 2)
 
         ax1 = plt.subplot(211)
+        plt.title("Progress at time k = {}".format(self.k))
         plt.xlim([-self.k, self.Hp])
 
         for s in range(0, self.states):
@@ -230,8 +252,9 @@ class MpcObj:
                 plt.plot(t[1:self.k + 1], self.get_x0_log()[s, 0:].T, color=col)
                 plt.plot(t[2:self.k + 2], self.get_expected_x_log()[s, 0:].T, color=col, ls='-.')
 
-                plt.plot(t[self.k + 1:], self.predicted_states[s::self.states], color=col)
-                plt.plot(t[self.k + 1:], self.ref[s::self.states], '--', color=col)
+                plt.plot(t[self.k + 1:], self.predicted_states[s::self.states], color=col, alpha=0.6)
+                plt.plot(t[self.k + 1:], self.ref[s::self.states], '--', color=col,alpha=0.6)
+                plt.plot(t[2:self.k+2],self.get_ref_log()[s,:].T, '--', color=col)
                 plt.plot(t[self.k:self.k + 2], ca.vertcat(self.x0[s], self.predicted_states[s]), 'r-')
                 plt.plot(t[self.k], self.x0[s], '.', color=col, label="State {}={:.3f}".format(s, float(self.x0[s])))
 
@@ -239,32 +262,35 @@ class MpcObj:
         ax1.legend()
         ax2 = plt.subplot(212, sharex=ax1)
 
-        t = np.append(np.arange(-1, 0), t)
         for i in range(0, self.inputs):
             col = plt.cm.tab10(i)
             w = 'post'
             if not ignore_inputs.__contains__(i):
-                if opts['drawU'] == 'U':
+                if opts['drawU'] == 'both' or opts['drawU'] == 'U':
 
-                   # plt.plot(t[1:self.k + 1], self.get_x0_log()[s, 0:].T, color=col)
+                    # plt.plot(t[1:self.k + 1], self.get_x0_log()[s, 0:].T, color=col)
 
                     U = np.cumsum(ca.vertcat(self.get_dU_log()[i, 0:-1].T, self.dU[i::self.inputs]))
 
-                    plt.step(t[1:self.k + self.Hu ], U, color=col, where=w)
+                    plt.step(t[0:self.k + self.Hu], U, color=col, where=w)
                     #       plt.step(t[1:3], U[1:3], color=col, where=w)
-                    plt.plot(t[self.k + 1], U[self.k], '.', color=col, label="U{}={:.3f}".format(i, float(U[self.k])))
+                    plt.plot(t[self.k], U[self.k], '.', color=col, label="U{}={:.3f}".format(i, float(U[self.k])))
 
-                    plt.step(t[self.k + 1:self.k + 3], U[self.k-1:self.k + 1], 'r-', where="pre")
+                    plt.step(t[self.k:self.k + 2], U[self.k - 1:self.k + 1], 'r-', where="pre")
 
-                else:
-                    idU = self.dU[i::self.inputs]
-                    plt.plot(t[self.k + 0:self.k + self.Hu], idU, '.', color=col)
+                if opts['drawU'] == 'both' or opts['drawU'] == 'dU':
+                    idU = ca.vertcat(self.get_dU_log()[i, 0:-1].T, self.dU[i::self.inputs])
+
+                    plt.plot(t[0:self.k + self.Hu], idU, '.', color=col)
                     # plt.plot(t[0:2], ca.vertcat(self.u_prev[i], idU[0]), '.', color=col)
-                    plt.plot(t[self.k + 1], idU[self.k], 'o', color=col, label="dU{}={:.3f}".format(i, idU[self.k]))
+                    plt.plot(t[self.k ], idU[self.k], 'o', color=col, label="dU{}={:.3f}".format(i, float(idU[self.k])))
                     # plt.plot(t[0], idU[0], 'r.')
         ax2.legend()
+
         plt.xlabel('Time in steps')
         plt.ylabel(opts['drawU'])
+        plt.draw()
+        plt.pause(0.1)
         plt.show()
 
     # def step(self):
